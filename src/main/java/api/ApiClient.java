0c1eee4d0f57422f19308b45e415a8cf2eb3a300
Vladimir Anisimov <vanisimov@kode-t.ru>
2020-04-20 02:00:16 +0700

    Fix static block init

--- src/main/java/api/ApiClient.java
+++ src/main/java/api/ApiClient.java
@@ -7,18 +7,24 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private final static OkHttpClient client = new OkHttpClient
-            .Builder()
-            //.addInterceptor()
-            .build();
+    private final static OkHttpClient client;
+    private final static Retrofit retrofit;
+
+    static {
+        //TODO добавить interceptor для добавления хедеров
+        client = new OkHttpClient
+                .Builder()
+                //.addInterceptor()
+                .build();
 //            .addHeader("x-rapidapi-host", "deezerdevs-deezer.p.rapidapi.com")
 //            .addHeader("x-rapidapi-key", "b73a9146dcmshb911597d909a091p12e882jsnf27a31953b60")
 
-    private final static Retrofit retrofit = new Retrofit
-            .Builder()
-            .client(client)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
+        retrofit = new Retrofit
+                .Builder()
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
 
     public static final ApiDeezer apiDeezer = retrofit.create(ApiDeezer.class);
     public static final ApiTheAudioDb apiTheAudioDb = retrofit.create(ApiTheAudioDb.class);
